07ed63bb26d987d90271d64997d9257d059ecc00
Franco Reyes <z5359122@ad.unsw.edu.au>
2022-01-11 11:15:40 +1100

    Fully functional for non-nested conditions

--- handbook.py
+++ handbook.py
@@ -21,7 +21,7 @@ with open("./conditions.json") as f:
     CONDITIONS = json.load(f)
     f.close()
 
-def parse_prereqs(course):
+def parse_prereqs(course: str):
     """
     Given a course code, return a list with a set of conditions that must each be met
     These conditions are either:
@@ -39,26 +39,42 @@ def parse_prereqs(course):
         conditions = conditions[conditions.index(':') + 1:]
 
     # First split by ANDs
-    and_conditions = [s.strip("( ).") for s in conditions.split("AND")]
+    and_conditions = [s.strip("( ).,") for s in conditions.split("AND")]
 
     for ac in and_conditions:
         current_and = []
         # Now split by OR and add to a list in the list
-        or_conditions = [s.strip("( ).") for s in ac.split("OR")]
+        or_conditions = [s.strip("( ).,") for s in ac.split("OR")]
         for oc in or_conditions:
             if is_course_code(oc):
                 current_and.append(oc)
+            elif "UNITS" in oc:
+                oc_split = oc.split()
+                uoc_required = int(oc_split[oc_split.index("UNITS") - 1])
+                uoc_conditions = []
+                if "IN" in oc:
+                    if "LEVEL" in oc:
+                        level_idx = oc_split.index("LEVEL")
+                        uoc_conditions.append(oc_split[level_idx + 2] + oc_split[level_idx + 1])
+                    else:
+                        for course in oc_split[oc_split.index("IN") + 1:]:
+                            uoc_conditions.append(course.strip("( ).,"))
+                else:
+                    uoc_conditions.append("")
+
+                current_and.append((uoc_required // 6, uoc_conditions))
 
         if current_and != []:
             prereqs.append(current_and)
 
     return prereqs
 
-def is_course_code(s):
+def is_course_code(s: str):
     '''
     Given a string, return whether or not it is a course code
     '''
-    return len(s) == 8 and s[0:4].isupper() and s[4:8].isnumeric()
+    return (len(s) == 8 and s[0:4].isupper() and s[4:8].isnumeric()) or \
+           (len(s) == 4 and s.isnumeric())
 
 def is_unlocked(courses_list, target_course):
     """Given a list of course codes a student has taken, return true if the target_course 
@@ -71,11 +87,31 @@ def is_unlocked(courses_list, target_course):
     """
     prereqs = parse_prereqs(target_course)
 
-    return all(any(x in y for x in courses_list) for y in prereqs)
+    print(prereqs)
+
+    for pr in prereqs:
+        for item in pr:
+            if isinstance(item, str):
+                if not any(x in y for x in pr for y in courses_list):
+                    return False
+            # if isinstance(item, list):
+            #     for l in pr:
+            #         if not any(x in y for x in l for y in courses_list):
+            #             return False
+            if isinstance(item, tuple):
+                matches = 0
+                for substring in item[1]:
+                    for course_code in courses_list:
+                        if substring in course_code:
+                            matches += 1
+                if matches < item[0]:
+                    return False
+
+    return True
 
 
 if __name__ == '__main__':
     print(parse_prereqs(sys.argv[1]))
-    # print(is_unlocked(["COMP1521", "COMP1531", "COMP1511", "COMP2521"], "COMP1511"))
+    # print(is_unlocked(["COMP1521", "COMP1531", "COMP1511", "COMP2521", "COMP2511", "COMP2000"], "COMP3901"))
 
     
